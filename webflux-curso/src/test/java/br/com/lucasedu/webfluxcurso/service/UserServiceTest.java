f4d3aca5d42b42d1c16bad7aa2090cb442af85b7
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2023-05-24 17:03:34 -0300

    Added test unit of method findAll

--- webflux-curso/src/test/java/br/com/lucasedu/webfluxcurso/service/UserServiceTest.java
+++ webflux-curso/src/test/java/br/com/lucasedu/webfluxcurso/service/UserServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -57,5 +58,19 @@ class UserServiceTest {
                 .verify();
 
         verify(repository, times(1)).findById(anyString());
+    }
+
+    @Test
+    void testeFindAll() {
+        when(repository.findAll()).thenReturn(Flux.just(User.builder().build()));
+
+        Flux<User> result = service.findAll();
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        verify(repository, times(1)).findAll();
     }
 }
